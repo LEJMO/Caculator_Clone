8e67aca47f84a1e89e7d833b9daa2cb6e18d8a76
bethev(leejunmo) <34985422+LEJMO@users.noreply.github.com>
2022-05-27 16:23:54 +0900

    세팅

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by leejunmo on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
