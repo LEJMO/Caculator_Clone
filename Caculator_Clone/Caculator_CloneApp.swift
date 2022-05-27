d7b18c782dd5e0761713bd01c8b3bf444ec883df
bethev(leejunmo) <34985422+LEJMO@users.noreply.github.com>
2022-05-27 13:52:17 +0900

    프로젝트생성

---
+++ Caculator_Clone/Caculator_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caculator_CloneApp.swift
+//  Caculator_Clone
+//
+//  Created by leejunmo on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct Caculator_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
